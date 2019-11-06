ba5d7d59a394bceb91bafd4a18587ec099600d4b
Yang Chen Long <820371826@qq.com>
2019-11-06 20:29:26 +0800

    add hello.c

--- hello.c
+++ hello.c
@@ -1,14 +1,60 @@
 #include<stdio.h>
-#define N 100
+#include <string.h>
+#define N 80
 int main()
-{	int i=0,b=0;
-    char a[N];
-	gets(a);
-    for(i=0;i<N;i++)
+{	int ac=1,bc=2,i,j,n=0,m=0,c=0,d=0;
+    char a[N][N] ,b[]="End of file";
+	gets(a[c]);
+#if 0
+	for(c=0;c<N;c++)
 	{
-		if((a[i]>='A'&&a[i]<='Z')||(a[i]>='a'&&a[i]<='z'))
-			b=b+1;
+		 gets(a[c]);
+		 
+		 if(!strcmp(a[c], "End of file"))
+		 {
+			 break;		 
+		 }else{d++;}
 	}
-	printf("%d",b);
+#else
+	while(strcmp(a[c], "End of file"))
+	{
+		 gets(a[c]);
+		 
+		 d++;
+	}
+#endif
+	for(c=0;c<d;c++)
+	{
+		  n=0;
+			  for(i=0;i<N;i++)
+			  {
+				if(a[c][i]=='\0')
+					break;
+				else
+				  n=n+1;
+			  }
+
+				for(i=0;i<n;i++)
+				{	  m=0;
+					  if(a[c][i]==' ')
+					  {
+						 m=m+1;
+						 for(j=i;j<n;j++)
+						 {
+				 			 if(a[c][i]!='\0')
+							 {
+			   					 a[c][j]=a[c][j+1];
+							 }
+						 }
+					  }			  
+					}
+	 	
+		for(i=0;i<=(n-m);i++)
+		{
+			printf("%c",a[c][i]);
+		}
+		printf("\n");
+	}
+	
     return 0;
 }
