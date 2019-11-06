f2991f53e773dc75c0884da7e416e603aa82af79
Yang Chen Long <820371826@qq.com>
2019-11-07 00:22:42 +0800

    add hello.c

--- hello.c
+++ hello.c
@@ -1,69 +1,21 @@
 #include<stdio.h>
-#include <string.h>
-#define N 80
+#define N 100
 int main()
-{	int i,j,n=0,m=0,c=0,d=0;
-    char a[N][N] ,b[]="End of file";
-	gets(a[c]);
-
-	while(strcmp(a[c], "End of file")&&(c<N))
-	{	 c++;
-		 d++;
-		 gets(a[c]);		 
-	} 
-	for(c=0;c<d;c++)
+{char a[N];
+    int b=0,i=0;
+    gets(a);
+	for(i=0;i<N;i++)
 	{
-			  n=0;
-			  for(i=0;i<N;i++)
-			  {
-				if(a[c][i]=='\0')
-					break;
-				else if(!((a[c][i]>='0'&&a[c][i]<='9')||a[c][i]==' '||(a[c][i]>='A'&&a[c][i]<='Z')||(a[c][i]>='a'&&a[c][i]<='z')))
-					return 0;
-				else
-					n=n+1;
-			  }
-			    m=0;
-				for(i=0;i<n;i++)
-				{	  
-					  if(a[c][i]==' ')
-					  {
-						 m=m+1;
-						 for(j=i;j<n;j++)
-						 {
-				 			 if(a[c][i]!='\0')
-							 {
-			   					 a[c][j]=a[c][j+1];
-							 }
-						 }
-						 i=i-1;
-					  }			  
-				}	 	
-		for(i=0;i<(n-m);i++)
-		{
-			printf("%c",a[c][i]);
-		}
-		printf("\n");
-	}	
+		if(a[i]=='\0')
+			break;
+		else
+			b++;
+	}
+	for(i=0;i<b;i++)
+	{
+		if((a[i]>='a'&&a[i]<='z')||(a[i]>='A'&&a[i]<='Z'))
+			a[i]=a[i]+1;
+	 printf("%c",a[i]);
+	}
     return 0;
 }
-
-
-#if 0
-	for(c=0;c<N;c++)
-	{
-		 gets(a[c]);
-		 
-		 if(!strcmp(a[c], "End of file"))
-		 {
-			 break;		 
-		 }else{d++;}
-	}
-//#else
-	while(strcmp(a[c], "End of file"))
-	{	 c++;
-		 gets(a[c]);
-		 
-		 d++;
-	}
-#endif
