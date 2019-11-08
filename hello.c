bce0001ea58d2df4a5621000db5167f03ae2556d
Yang Chen Long <820371826@qq.com>
2019-11-08 23:02:21 +0800

    add hello.c

--- hello.c
+++ hello.c
@@ -5,45 +5,28 @@ struct tong{
      char name[10];
      int  score[3];
 };
+void input_file(struct tong p[M],int N);
+void print_file(struct tong q[M],int N);
 int main()
 {
  struct   tong stu[M];
- int aa[M],bb[M];
- int i, N,a=0,b=0,c=0,max=0,t=0,n=0;
+ int N;
  scanf("%d",&N);
- for(i=0;i<N;i++)
- {
-	 scanf("%s %s %d %d %d",stu[i].stunum,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2]);
- }
-
-for(i=0;i<N;i++)
-{
-	a=stu[i].score[0]+a;
-	b=stu[i].score[1]+b;
-	c=stu[i].score[2]+c;
+ input_file(stu,N);
+ print_file(stu,N);
 }
 
-printf("%d %d %d\n",a/N,b/N,c/N);
-
-for(i=0;i<N;i++)
-{
-	 aa[i]=stu[i].score[0]+stu[i].score[1]+stu[i].score[2]; 
-	 bb[i]=stu[i].score[0]+stu[i].score[1]+stu[i].score[2]; 
- }
- for(i=0;i<N; i++)
- {
-	if(aa[i]>max)
-	{
-		t=max;
-		max=aa[i];
-		aa[i]=t;
-	}
- }
+void input_file(struct tong p[M],int N)
+{int i;
  for(i=0;i<N;i++)
  {
-	if(bb[i]==max)
-	{
-		printf("%s %s %d %d %d",stu[i].stunum,stu[i].name,stu[i].score[0],stu[i].score[1],stu[i].score[2]);
-	}
+	 scanf("%s %s %d %d %d",p[i].stunum,p[i].name,&p[i].score[0],&p[i].score[1],&p[i].score[2]);
+ }
+}
+void print_file(struct tong q[M],int N)
+{int i;
+ for(i=0;i<N;i++)
+ {
+		printf("%s,%s,%d,%d,%d\n",q[i].stunum,q[i].name,q[i].score[0],q[i].score[1],q[i].score[2]);
  }
 }
