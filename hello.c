22d49f9cbd2fbb9f99e52d7e50381e6014098a3f
Yang Chen Long <820371826@qq.com>
2019-11-21 21:43:31 +0800

    add hello.c

--- hello.c
+++ hello.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
-#include <math.h>
-#define S(a,b,c)		(a+b+c)/2
-#define AREA(a,b,c,S)	sqrt(S*(S-a)*(S-b)*(S-c))
+#define LEAP_YEAR(y)  if(((y%4==0)&&(y%100!=0))||(y%400==0))\
+					  {\
+						  printf("L");\
+					  }\
+					  else\
+					  {\
+						  printf("N");\
+					  }
 int main()
 {
-    float a,b,c;
-    scanf("%f %f %f",&a,&b,&c);
-
-	printf("%.3f",AREA(a,b,c,S(a,b,c)));
+    int a;
+    scanf("%d",&a);
+	LEAP_YEAR(a);
 
     return 0;
 }
